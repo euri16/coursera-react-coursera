6a04fee431c34c86b912d7f2b956dcfaea21cd0b
Eury Perez <eury@instacarro.com>
2018-07-19 13:02:08 -0400

    Uncontrolled Form

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -6,10 +6,12 @@ import { DISHES } from '../shared/dishes';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
+import About from './AboutComponent';
 
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
+import Contact from './ContactComponent';
 
 class Main extends Component {
 
@@ -34,8 +36,7 @@ class Main extends Component {
                 <Home
                     dish={this.state.dishes.filter((dish) => dish.featured)[0]}
                     promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
-                    leader={this.state.leaders.filter((leader) => leader.featured)[0]}
-                />
+                    leader={this.state.leaders.filter((leader) => leader.featured)[0]} />
             );
         }
 
@@ -45,12 +46,23 @@ class Main extends Component {
                     comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))} />
             );
         };
+        
+        const AboutPage = () => {
+            return <About leaders={this.state.leaders} />
+        }
+        
+        const ContactPage = () => {
+            return <Contact />
+        }
+
         return (
             <div>
                 <Header />
                 <Switch>
                     <Route path='/home' component={HomePage} />
                     <Route path='/menu/:dishId' component={DishWithId} />
+                    <Route path='/aboutus' component={AboutPage} />
+                    <Route path='/contactus' component={ContactPage} />
                     <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
                     <Redirect to="/home" />
                 </Switch>
